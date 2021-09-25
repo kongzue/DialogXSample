eaf9a5ae60aae76390f961c96871c17d16b44ee4
kongzue <myzcxhh@live.cn>
2021-09-25 12:31:39 +0800

    update@20210925.01

--- app/src/main/java/com/kongzue/dialogxsimpledemo/MainActivity.java
+++ app/src/main/java/com/kongzue/dialogxsimpledemo/MainActivity.java
@@ -1,8 +1,14 @@
 package com.kongzue.dialogxsimpledemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,12 +25,42 @@ public class MainActivity extends AppCompatActivity {
     
     public void onCityPicker(View view) {
         CityPickerDialog.build()
-                .setDefaultSelect("陕西省","西安市","未央区")
+                .setDefaultSelect("陕西省", "西安市", "未央区")
                 .show(new OnCitySelected() {
-            @Override
-            public void onSuccess(String text, String province, String city, String district) {
-                ((Button) view).setText(text);
-            }
-        });
+                    @Override
+                    public void onSuccess(String text, String province, String city, String district) {
+                        ((Button) view).setText(text);
+                    }
+                });
+    }
+    
+    public void onDatePicker(View view) {
+    
+    }
+    
+    public void onShare(View view) {
+    
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_github:
+                try {
+                    Uri uri = Uri.parse("https://github.com/kongzue/DialogXSimple");
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }catch (Exception e){}
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
